be7a0a090b62b0b2f1d5496685315f0fcf02bd7d
Rupin Raveendra Nath <rupinr@live.com>
2020-05-21 11:44:00 +0200

    Aggregate Result

---
+++ src/main/java/log/aggregate/AggregationProcessor.java
@@ -0,0 +1,66 @@
+package log.aggregate;
+
+import log.extractor.model.LogEntry;
+import log.extractor.model.Stage;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AggregationProcessor {
+
+   static DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss','SSS");
+   public static Map<String,Integer> serviceCallCounter = new HashMap<>();
+   public static Map<String,Double> maxExecutionTime = new HashMap<>();
+
+    public static void aggregate(List<LogEntry> logEntries) {
+        logEntries.forEach(logEntry -> {
+            if(serviceCallCounter.containsKey(logEntry.getServiceName())){
+                serviceCallCounter.put(logEntry.getServiceName(), serviceCallCounter.get(logEntry.getServiceName())+1);
+            }
+            else {
+                serviceCallCounter.put(logEntry.getServiceName(),1);
+            }
+            double currentExecutionTime = getExecutionTime(logEntries, logEntry.getServiceName(), logEntry.getUniqueId());
+            if(maxExecutionTime.containsKey(logEntry.getServiceName())) {
+
+                if(currentExecutionTime>maxExecutionTime.get(logEntry.getServiceName())){
+                    maxExecutionTime.put(logEntry.getServiceName(), currentExecutionTime);
+                }
+            }
+            else {
+                maxExecutionTime.put(logEntry.getServiceName(), currentExecutionTime);
+            }
+        });
+    }
+
+    private static double getExecutionTime(List<LogEntry> logEntries, String serviceName, long requestId) {
+        double runTime=0;
+        LogEntry entry = logEntries.stream()
+                .filter(
+                        item -> item.getServiceName()
+                                .equals(serviceName)
+                                && item.getUniqueId() == requestId
+                                && item.getStage() == Stage.ENTRY)
+                .findFirst().get();
+
+        LogEntry exit = logEntries.stream()
+                .filter(
+                        item -> item.getServiceName()
+                                .equals(serviceName)
+                                && item.getUniqueId() == requestId
+                                && item.getStage() == Stage.EXIT)
+                .findFirst().get();
+
+        try {
+            runTime  = (dateFormat.parse(exit.getTimeStamp()).getTime() - dateFormat.parse(entry.getTimeStamp()).getTime())/1000.0d;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return runTime;
+    }
+}
--- src/main/java/log/processor/MainClass.java
+++ src/main/java/log/processor/MainClass.java
@@ -1,6 +1,7 @@
 package log.processor;
 
 import file.reader.FileReader;
+import log.aggregate.AggregationProcessor;
 import log.extractor.model.LogEntry;
 
 import java.io.IOException;
@@ -12,6 +13,13 @@ public class MainClass {
     public static void main(String[] args) throws IOException {
         List<LogEntry> logEntryList = new ArrayList<>();
         FileReader.readFile("test.log",logEntryList);
-        logEntryList.forEach(System.out::println);
+        AggregationProcessor.aggregate(logEntryList);
+
+        AggregationProcessor.serviceCallCounter.keySet().forEach(
+                serviceName -> {
+                    System.out.println(serviceName + " has been called "+ AggregationProcessor.serviceCallCounter.get(serviceName)/2 +" times");
+                    System.out.println("Max Execution Time is "+ AggregationProcessor.maxExecutionTime.get(serviceName)+"\n");
+                }
+        );
     }
 }
